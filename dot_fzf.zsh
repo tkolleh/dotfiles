dcbded5c87739a5589c25a1331a2126d3de67514
tkolleh <tinatuh@gmail.com>
2020-10-04 23:00:51 -0400

    Add bash profile and rc files

---
+++ dot_fzf.zsh
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */Users/tkolleh/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/Users/tkolleh/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/Users/tkolleh/.fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/Users/tkolleh/.fzf/shell/key-bindings.zsh"
