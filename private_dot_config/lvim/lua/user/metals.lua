e99b1bc76d981c8daa4912346e8ef4a596261ef1
TJ Kolleh <tinatuh@gmail.com>
2022-04-26 19:15:09 -0400

    Metals related key mappings

--- private_dot_config/lvim/lua/user/metals.lua
+++ private_dot_config/lvim/lua/user/metals.lua
@@ -11,15 +11,35 @@ M.config = function()
     ".scala-build",
   }
   metals_config.settings = {
-    showImplicitArguments = true,
-    showInferredType = true,
     ammoniteJvmProperties = {
       "-Xmx2G", "-Xms256M"
     },
-    excludedPackages = {},
+    bloopJvmProperties = { "-Xss8m" },
+    showImplicitArguments = true,
+    showImplicitConversionsAndClasses = true,
+    showInferredType = true,
+    excludedPackages = {
+      "akka.actor.typed.javadsl",
+      "com.github.swagger.akka.javadsl",
+      "akka.stream.javadsl",
+      "akka.http.javadsl",
+    },
+    fallbackScalaVersion = "2.12.15",
+    serverVersion = "latest.snapshot",
   }
   metals_config.init_options.statusBarProvider = "on"
+
   require("metals").initialize_or_attach { metals_config }
+
+
+  lvim.builtin.which_key.mappings["m"] = {
+    name = "metals",
+    c = { "<cmd>lua require('telescope').extensions.metals.commands()", "Commands"},
+    w = { "<cmd>lua require('metals').hover_worksheet()", "Worksheet"},
+    t = { "<cmd>lua require('metals.tvp').toggle_tree_view()", "Tree view"},
+    r = { "<cmd>lua require('metals.tvp').reveal_in_tree()", "Reveal in tree"},
+    s = { "<cmd>lua require('metals').toggle_setting('showImplicitArguments')", "Show implicit arguments"},
+  }
 end
 
 return M
