7ea6735cc56fc7ceb9b3307401b279e2467272b2
TJ Kolleh <tinatuh@gmail.com>
2022-04-26 19:15:09 -0400

    Metals related key mappings

--- private_dot_config/lvim/config.lua
+++ private_dot_config/lvim/config.lua
@@ -12,6 +12,7 @@ an executable
 lvim.log.level = "debug"
 lvim.format_on_save = true
 vim.o.timeoutlen = 700
+vim.o.laststatus = 3
 
 vim.opt_global.shortmess:remove("F")
 
@@ -29,44 +30,6 @@ lvim.builtin.lualine.options = {
   theme = 'material',
 }
 
--- keymappings [view all the defaults by pressing <leader>Lk]
-lvim.leader = "space"
--- add your own keymapping
-lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
-
----- search highlighted text
-lvim.keys.visual_mode["//"] = 'y/<C-R>"<CR>'
-
--- unmap a default keymapping
--- lvim.keys.normal_mode["<C-Up>"] = ""
--- edit a default keymapping
--- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
-
-
--- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
--- lvim.builtin.telescope.on_config_done = function()
---   local actions = require "telescope.actions"
---   -- for input mode
---   lvim.builtin.telescope.defaults.mappings.i["<C-j>"] = actions.move_selection_next
---   lvim.builtin.telescope.defaults.mappings.i["<C-k>"] = actions.move_selection_previous
---   lvim.builtin.telescope.defaults.mappings.i["<C-n>"] = actions.cycle_history_next
---   lvim.builtin.telescope.defaults.mappings.i["<C-p>"] = actions.cycle_history_prev
---   -- for normal mode
---   lvim.builtin.telescope.defaults.mappings.n["<C-j>"] = actions.move_selection_next
---   lvim.builtin.telescope.defaults.mappings.n["<C-k>"] = actions.move_selection_previous
--- end
-
--- Use which-key to add extra bindings with the leader-key prefix
--- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
--- lvim.builtin.which_key.mappings["t"] = {
---   name = "+Trouble",
---   r = { "<cmd>Trouble lsp_references<cr>", "References" },
---   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
---   d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnostics" },
---   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
---   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
---   w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnostics" },
--- }
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
@@ -93,32 +56,6 @@ lvim.builtin.treesitter.ensure_installed = {
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
-
--- generic LSP settings
--- you can set a custom on_attach function that will be used for all the language servers
--- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
--- lvim.lsp.on_attach_callback = function(client, bufnr)
---   local function buf_set_option(...)
---     vim.api.nvim_buf_set_option(bufnr, ...)
---   end
---   --Enable completion triggered by <c-x><c-o>
---   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
--- end
--- you can overwrite the null_ls setup table (useful for setting the root_dir function)
--- lvim.lsp.null_ls.setup = {
---   root_dir = require("lspconfig").util.root_pattern("Makefile", ".git", "node_modules"),
--- }
--- or if you need something more advanced
--- lvim.lsp.null_ls.setup.root_dir = function(fname)
---   if vim.bo.filetype == "javascript" then
---     return require("lspconfig/util").root_pattern("Makefile", ".git", "node_modules")(fname)
---       or require("lspconfig/util").path.dirname(fname)
---   elseif vim.bo.filetype == "php" then
---     return require("lspconfig/util").root_pattern("Makefile", ".git", "composer.json")(fname) or vim.fn.getcwd()
---   else
---     return require("lspconfig/util").root_pattern("Makefile", ".git")(fname) or require("lspconfig/util").path.dirname(fname)
---   end
--- end
 
 -- set a formatter if you want to override the default lsp one (if it exists)
 local formatters = require "lvim.lsp.null-ls.formatters"
@@ -204,13 +141,39 @@ lvim.plugins = {
 --   -- tmux
    {'christoomey/vim-tmux-navigator'},
 
--- notes
--- {'renerocksai/telekasten.nvim'},
---  {
---    "folke/trouble.nvim",
---    cmd = "TroubleToggle",
---  },
+--   -- Custom Quickfix window
+    {
+      "kevinhwang91/nvim-bqf",
+      event = { "BufRead", "BufNew" },
+      config = function()
+      require("bqf").setup({
+              auto_enable = true,
+              preview = {
+              win_height = 12,
+              win_vheight = 12,
+              delay_syntax = 80,
+              border_chars = { "┃", "┃", "━", "━", "┏", "┓", "┗", "┛", "█" },
+              },
+              func_map = {
+              vsplit = "",
+              ptogglemode = "z,",
+              stoggleup = "",
+              },
+              filter = {
+              fzf = {
+              action_for = { ["ctrl-s"] = "split" },
+              extra_opts = { "--bind", "ctrl-o:toggle-all", "--prompt", "> " },
+              },
+              },
+              })
+      end,
+    },
 
+--  -- Telescope symbols populator
+  {'nvim-telescope/telescope-symbols.nvim'},
+
+
+--  end additional plugins bloc
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
@@ -223,6 +186,16 @@ lvim.plugins = {
 lvim.autocommands.custom_groups = {
   { "FileType", "java,scala,sbt", "lua require('user.metals').config()" }
 }
+
+-- keymappings [view all the defaults by pressing <leader>Lk]
+lvim.leader = "space"
+
+-- Write buffer (Save changes)
+-- lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+
+---- search highlighted text
+lvim.keys.visual_mode["//"] = 'y/<C-R>"<CR>'
+
 
 lvim.builtin.which_key.mappings["x"] = {
   name = "+ripgrep search",
