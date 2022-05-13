6e72fd560d00baea7d626f7a9963e5a1e949c57c
TJ Kolleh <tinatuh@gmail.com>
2022-05-13 17:58:46 -0400

    Dont use vi mode

---
+++ private_dot_config/lvim/lua/user/gitlinker.lua
@@ -0,0 +1,29 @@
+local M = {}
+
+M.config = function ()
+  local gitlinker = require("gitlinker")
+  gitlinker.setup({
+    opts = {
+      -- force the use of a specific remote
+      remote = nil,
+      -- adds current line nr in the url for normal mode
+      add_current_line_on_normal_mode = true,
+      -- callback for what to do with the url
+      action_callback = gitlinker.actions.copy_to_clipboard,
+      -- print the url after performing the action
+      print_url = true,
+    },
+    callbacks = {
+          ["code%.corp%.creditkarma%.com"] = function(url_data)
+              url_data.host = "code.corp.creditkarma.com"
+              return gitlinker.hosts.get_github_type_url(url_data)
+            end,
+    },
+    -- default mapping to call url generation with action_callback
+    mappings = nil
+  })
+  lvim.builtin.which_key.mappings['gy'] = {'<cmd>lua require("gitlinker").get_buf_range_url("n", {action_callback = require"gitlinker.actions".copy_to_clipboard})<cr>', "Copy gitlink"}
+  lvim.keys.visual_mode['gy'] = '<cmd>lua require("gitlinker").get_buf_range_url("v", {action_callback = require"gitlinker.actions".copy_to_clipboard})<cr>'
+end
+
+return M
