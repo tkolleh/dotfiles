bbd935c246487ae72e8cd05ad2a6a58c1e28f2f3
TJ Kolleh <tinatuh@gmail.com>
2022-05-16 09:42:16 -0400

    fix metal configuration

--- private_dot_config/lvim/lua/user/metals.lua
+++ private_dot_config/lvim/lua/user/metals.lua
@@ -33,21 +33,10 @@ M.config = function()
   metals_config.init_options.statusBarProvider = "on"
 
   metals_config.on_attach = function(client, bufnr)
+    require("lvim.lsp").common_on_attach(client, bufnr)
     require("metals").setup_dap()
   end
-
-  -- Autocmd that will actually be in charging of starting the whole thing
-  local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
-  vim.api.nvim_create_autocmd("FileType", {
-    -- NOTE: You may or may not want java included here. You will need it if you
-    -- want basic Java support but it may also conflict if you are using
-    -- something like nvim-jdtls which also works on a java filetype autocmd.
-    pattern = { "scala", "sbt", "java" },
-    callback = function()
-      require("metals").initialize_or_attach(metals_config)
-    end,
-    group = nvim_metals_group,
-  })
+  require("metals").initialize_or_attach { metals_config }
 
   lvim.builtin.which_key.mappings["m"] = {
     name = "metals",
