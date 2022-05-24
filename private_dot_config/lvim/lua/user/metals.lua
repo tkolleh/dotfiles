e3fb2654adba766ea03e69b9220cd0bc7f59ccd3
TJ Kolleh <tinatuh@gmail.com>
2022-05-24 00:35:04 -0400

    Add metals status to lualine

--- private_dot_config/lvim/lua/user/metals.lua
+++ private_dot_config/lvim/lua/user/metals.lua
@@ -46,7 +46,18 @@ M.config = function()
     require("metals").setup_dap()
   end
 
-  require("metals").initialize_or_attach(metals_config)
+  -- Autocmd that will actually be in charging of starting the whole thing
+   local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+   vim.api.nvim_create_autocmd("FileType", {
+     -- NOTE: You may or may not want java included here. You will need it if you
+     -- want basic Java support but it may also conflict if you are using
+     -- something like nvim-jdtls which also works on a java filetype autocmd.
+     pattern = { "scala", "sbt", "java" },
+     callback = function()
+       require("metals").initialize_or_attach(metals_config)
+     end,
+     group = nvim_metals_group,
+   })
 
   lvim.builtin.which_key.mappings["m"] = {
     name = "metals",
@@ -57,14 +68,19 @@ M.config = function()
     s = { "<cmd>lua require('metals').toggle_setting('showImplicitArguments')<cr>", "Show implicit arguments"},
   }
 
-  local function metals_status()
-    return vim.g["metals_status"] or ""
-  end
-
-  lvim.builtin.lualine.sections.lualine_c = {
-    metals_status(),
-  }
-
 end
+
+M.metals_status = function()
+  local conditions = require("lvim.core.lualine.conditions")
+  local m_status = vim.pretty_print(vim.g["metals_status"]) or {}
+  local status = {
+    "g:metals_status",
+    icon = "",
+    color = { gui = "bold" },
+    cond = conditions.hide_in_width,
+  }
+  return status
+end
+
 
 return M
