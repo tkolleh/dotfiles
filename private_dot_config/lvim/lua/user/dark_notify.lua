c14e62ac3b6f382fb57034c65f6ffcae5459be1f
TJ Kolleh <tinatuh@gmail.com>
2022-06-05 17:43:17 -0400

    Style, function signature, and more

--- private_dot_config/lvim/lua/user/dark_notify.lua
+++ private_dot_config/lvim/lua/user/dark_notify.lua
@@ -2,15 +2,16 @@ local M = {}
 
 M.config = function()
   local dn = require('dark_notify')
+  local scheme = "vscode"
   dn.run({
     schemes = {
       -- you can use a different colorscheme for each
       dark  = {
-        colorscheme = "vscode",
+        colorscheme = scheme,
         background = "dark",
        },
       light = {
-        colorscheme = "vscode",
+        colorscheme = scheme,
         background = "light",
       }
     },
@@ -19,24 +20,21 @@ M.config = function()
       -- this is called at startup and every time dark mode is switched,
       -- either via the OS, or because you manually set/toggled the mode.
       -- mode is either "light" or "dark"
-      if mode == "dark" then
-        vim.g.tokyonight_style = "night"
+      if scheme == "tokyonight" then
+        vim.g.tokyonight_style = mode == "dark" and "night" or "day"
         vim.g.tokyonight_colors = {
           -- Default is too dark for "night" style
           border = "orange"
         }
-
-        -- Color scheme must be set last
-        vim.o.colorscheme = "vscode"
-        lvim.colorscheme = "vscode"
-
-      else
-        lvim.colorscheme = "vscode"
+      elseif scheme == "vscode" then
         vim.g.vscode_style = mode
         vim.g.vscode_italic_comment = 1
       end
-        vim.o.background = mode
-        lvim.background = mode
+
+      -- Color scheme must be set last
+      vim.o.background = mode
+      lvim.background = mode
+      lvim.colorscheme = scheme
     end,
   })
   dn.update()
