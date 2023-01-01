6d84138c85ee35934731aaeb6e1a1b81a096095f
tkolleh <tinatuh@gmail.com>
2023-01-01 00:09:40 -0500

    include iterm2 profile

--- private_dot_config/lvim/lua/user/metals.lua
+++ private_dot_config/lvim/lua/user/metals.lua
@@ -47,10 +47,10 @@ M.config = function()
 
   local capabilities = vim.lsp.protocol.make_client_capabilities()
   local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-  if status_ok then
-    capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
-    metals_config.capabilities = capabilities
-  end
+  -- if status_ok then
+  --   capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
+  --   metals_config.capabilities = capabilities
+  -- end
 
   metals_config.on_attach = function(client, bufnr)
     require("lvim.lsp").common_on_attach(client, bufnr)
