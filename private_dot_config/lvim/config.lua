3106d668e655f514489e9d42fe126b6df02c0dd7
TJ Kolleh <tinatuh@gmail.com>
2023-01-20 18:23:08 -0500

    Sync

--- private_dot_config/lvim/config.lua
+++ private_dot_config/lvim/config.lua
@@ -244,9 +244,28 @@ lvim.plugins = {
       config = function()
         require("lsp_signature").setup({
           floating_window = true,
+          floating_window_above_cur_line = true,
           doc_lines = 0,
           always_trigger = false,
           timer_interval = 100,
+          floating_window_off_x = 5, -- adjust float windows x position.
+          floating_window_off_y = function() -- adjust float windows y position. e.g. set to -2 can make floating window move up 2 lines
+            local linenr = vim.api.nvim_win_get_cursor(0)[1] -- buf line number
+            local pumheight = vim.o.pumheight
+            local winline = vim.fn.winline() -- line number in the window
+            local winheight = vim.fn.winheight(0)
+
+            -- window top
+            if winline - 1 < pumheight then
+              return pumheight
+            end
+
+            -- window bottom
+            if winheight - winline < pumheight then
+              return -pumheight
+            end
+            return 0
+          end,
       })
       end
     },
