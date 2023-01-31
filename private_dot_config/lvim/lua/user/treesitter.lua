516e8e8f86dcb1eff8126d002d4b7fdacdf97eb5
TJ Kolleh <tinatuh@gmail.com>
2023-01-30 21:16:59 -0500

    configure match it

--- private_dot_config/lvim/lua/user/treesitter.lua
+++ private_dot_config/lvim/lua/user/treesitter.lua
@@ -21,6 +21,16 @@ M.config = function ()
   lvim.builtin.treesitter.ignore_install = { "ocaml" }
   lvim.builtin.treesitter.highlight.enabled = true
   lvim.builtin.treesitter.playground.enable = true
+  lvim.builtin.treesitter.on_config_done = function(config)
+    config.setup {
+      matchup = {
+        enable = true,              -- mandatory, false will disable the whole extension
+        -- disable = { "c", "ruby" },  -- optional, list of language that will be disabled
+        -- disable_virtual_text: do not use virtual text to highlight the virtual end of a block, for languages without explicit end markers (e.g., Python).
+        -- [options]
+      },
+    }
+  end
 end
 
 return M
