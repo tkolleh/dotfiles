2ae77b17b4d1877547ebe7bab1f399bf1a0e55d5
TJ Kolleh <tinatuh@gmail.com>
2023-02-23 03:02:37 -0500

    A bunch of stuff

--- private_dot_config/lvim/lua/user/telescope.lua
+++ private_dot_config/lvim/lua/user/telescope.lua
@@ -1,23 +1,26 @@
+local h = require("user.helpers")
+
 local function setup_defaults()
   lvim.builtin.telescope.defaults = {
     file_ignore_patterns = { "target", "node_modules", "parser.c", "out", "%.min.js" },
     prompt_prefix = "❯",
     sorting_strategy = "ascending",
     layout_strategy = "horizontal",
-    path_display = { "smart" },
+    path_display = { "absolute" },
+    wrap_results = true,
     dynamic_preview_title = true,
     theme = "dropdown",
     layout_config = {
         horizontal = {
             prompt_position = "top",
-            preview_width = 0.55,
+            preview_width = 0.65,
             results_width = 0.8,
         },
         vertical = {
             mirror = false,
         },
-        width = 0.87,
-        height = 0.80,
+        width = 0.9,
+        height = 0.8,
         preview_cutoff = 120,
     },
   }
@@ -57,31 +60,26 @@ local function setup_mappings()
     -- for insert mode
     i = {
       ["<C-j>"] = actions.move_selection_next,
-      ["<C-k>"] = actions.move_selection_previous,
-      ["<C-h>"] = actions.cycle_history_prev,
+      ["<C-n>"] = actions.cycle_history_next,
+      ["<C-r>"] = actions.cycle_history_prev,
+      ["<c-h>"] = "which_key",
       ["<C-u>"] = false, -- clear input line rather than scroll preview
     },
     -- for normal mode
     n = {
-      ["<C-j>"] = actions.move_selection_next,
-      ["<C-k>"] = actions.move_selection_previous,
+      ["j"] = actions.move_selection_next,
+      ["k"] = actions.move_selection_previous,
+      ["q"] = actions.smart_send_to_qflist + actions.open_qflist,
+      ["Q"] = actions.smart_add_to_qflist + actions.open_qflist,
     },
   }
   --  -- Telescope LSP keymappings / keybindings
-  lvim.keys.normal_mode["<leader>ss"] = false
-  lvim.builtin.which_key.mappings["ss"] = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" }
-
-  lvim.keys.normal_mode["<leader>sS"] = false
-  lvim.builtin.which_key.mappings["sS"] = { "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", "Workspace Symbols"}
-
-  lvim.keys.normal_mode["<leader>sb"] = false
-  lvim.builtin.which_key.mappings["sb"] = { "<cmd>Telescope buffers<cr>", "Find Buffer" }
-
-  lvim.keys.normal_mode["<leader>sB"] = false
-  lvim.builtin.which_key.mappings["sB"] = { "<cmd>Telescope git_branches<cr>", "Checkout branch" }
+  h.which_key_set_key("ss", { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" })
+  h.which_key_set_key("sS", { "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", "Workspace Symbols"})
+  h.which_key_set_key("sb", { "<cmd>Telescope buffers<cr>", "Find Buffer" })
 
   -- keymappings / keybindings [view all the defaults by pressing <leader>Lk]
-  lvim.builtin.which_key.mappings["j"] = { "<cmd>Telescope jumplist<cr>", "Jump List" }
+  h.which_key_set_key("j", { "<cmd>Telescope jumplist<cr>", "Jump List" })
 
   vim.api.nvim_set_keymap(
     'n',
@@ -98,11 +96,26 @@ M.config = function()
   setup_mappings()
   --  -- See configuration details: https://github.com/LunarVim/LunarVim/issues/2426
   lvim.builtin.telescope.on_config_done = function(tele)
-    pcall(tele.load_extension, "dap")
-    pcall(tele.load_extension, "ui-select")
-    vim.cmd "autocmd User TelescopePreviewerLoaded setlocal wrap"
-    -- local opts = {}
-    -- tele.setup(opts)
+    -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
+    local _, actions = pcall(require, "telescope.actions")
+    tele.load_extension("dap")
+    tele.load_extension("ui-select")
+
+    local opts = {
+      buffers = {
+        sort_mru = true,
+        ignore_current_buffer = true,
+        mappings = {
+          i = {
+            ["<c-x>"] = actions.delete_buffer,
+          },
+          n = {
+            ["x"] = actions.delete_buffer,
+          },
+        },
+      },
+    }
+    tele.setup(opts)
   end
 end
 
