2752bf280e354a9b5e7552f142933c9a98b7600a
TJ Kolleh <tinatuh@gmail.com>
2023-02-26 20:23:33 -0500

    Configure telescope

--- private_dot_config/lvim/lua/user/telescope.lua
+++ private_dot_config/lvim/lua/user/telescope.lua
@@ -1,61 +1,77 @@
 local h = require("user.helpers")
 
+local function getActions()
+  local status_ok, actions = pcall(require, "telescope.actions")
+  if not status_ok then
+    vim.notify("telescope.actions not found", vim.log.levels.ERROR)
+    return {}
+  end
+  return actions
+end
+
+local actions = getActions()
+
 local function setup_defaults()
-  lvim.builtin.telescope.defaults = {
-    file_ignore_patterns = { "target", "node_modules", "parser.c", "out", "%.min.js" },
-    prompt_prefix = "❯",
-    sorting_strategy = "ascending",
-    layout_strategy = "horizontal",
-    path_display = { "absolute" },
-    wrap_results = true,
-    dynamic_preview_title = true,
-    theme = "dropdown",
-    layout_config = {
-        horizontal = {
-            prompt_position = "top",
-            preview_width = 0.65,
-            results_width = 0.8,
-        },
-        vertical = {
-            mirror = false,
-        },
-        width = 0.9,
-        height = 0.8,
-        preview_cutoff = 120,
+  lvim.builtin.telescope.defaults.file_ignore_patterns = { "target", "node_modules", "parser.c", "out", "%.min.js" }
+  lvim.builtin.telescope.defaults.prompt_prefix = "❯"
+  lvim.builtin.telescope.defaults.selection_caret = " "
+  lvim.builtin.telescope.defaults.sorting_strategy = "ascending"
+  lvim.builtin.telescope.defaults.layout_strategy = "horizontal"
+  lvim.builtin.telescope.defaults.path_display = { "absolute" }
+  lvim.builtin.telescope.defaults.wrap_results = true
+  lvim.builtin.telescope.defaults.dynamic_preview_title = true
+  lvim.builtin.telescope.defaults.theme = "dropdown"
+  lvim.builtin.telescope.defaults.mappings.i["<esc>"] = actions.close
+  lvim.builtin.telescope.defaults.winblend = 10
+
+  lvim.builtin.telescope.defaults.layout_config = {
+    -- prompt_position = "top",
+    height = 0.9,
+    width = 0.9,
+    bottom_pane = {
+      height = 25,
+      preview_cutoff = 120,
+    },
+    center = {
+      height = 0.7,
+      preview_cutoff = 40,
+      width = 0.9,
+    },
+    cursor = {
+      preview_cutoff = 40,
+    },
+    horizontal = {
+      width = 0.9,
+      preview_cutoff = 120,
+      preview_width = 0.6,
+    },
+    vertical = {
+      preview_cutoff = 40,
+    },
+    flex = {
+      flip_columns = 150,
     },
   }
 end
 
 local function setup_pickers()
-  -- Override default find_files picker to not show .git/ files but otherwise
+  for key, _ in pairs(lvim.builtin.telescope.pickers) do
+    if key ~= "planets" then
+      lvim.builtin.telescope.pickers[key].previewer = nil
+      lvim.builtin.telescope.pickers[key].theme = "dropdown"
+      lvim.builtin.telescope.pickers[key].layout_strategy = nil
+    end
+  end
+
+  lvim.builtin.telescope.pickers.git_files.previewer = nil
   -- show all files, including hidden ones (e.g. .gitignore, .gitmodule, etc are all useful).
-  lvim.builtin.telescope.defaults.pickers = {
-    find_files = {
-      -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
-      find_command = { "fd","--type=file","--hidden","--glob","!.git/*","--strip-cwd-prefix"},
-      theme = "dropdown",
-    },
-    live_grep = {
-      --@usage don't include the filename in the search results
-      only_sort_text = true,
-      theme = "dropdown",
-    },
-    buffers = {
-      only_sort_text = true,
-      theme = "dropdown"
-    },
-    oldfiles = {
-      theme = "dropdown"
-    },
-  }
+  -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+  lvim.builtin.telescope.defaults.pickers.find_files.find_command = { "fd","--type=file","--hidden","--glob","!.git/*","--strip-cwd-prefix"}
+  lvim.builtin.telescope.defaults.pickers.live_grep.only_sort_text = true
+  lvim.builtin.telescope.defaults.pickers.buffers.only_sort_text = true
 end
 
 local function setup_mappings()
-  local status_ok, actions = pcall(require, "telescope.actions")
-  if not status_ok then
-    vim.notify("telescope.actions not found", vim.log.levels.ERROR)
-    return
-  end
   lvim.builtin.telescope.defaults.mappings = {
     -- for insert mode
     i = {
@@ -102,6 +118,14 @@ M.config = function()
     tele.load_extension("ui-select")
 
     local opts = {
+      defaults = {
+        theme = "dropdown",
+        layout_config = {
+          prompt_position = "top",
+          width = 98,
+          height = 90,
+        }
+      },
       buffers = {
         sort_mru = true,
         ignore_current_buffer = true,
