4d02ad2f8a43306da482d6613c27a7a9078a2f87
TJ Kolleh <tinatuh@gmail.com>
2024-01-15 22:04:38 -0500

    correct data references

---
+++ private_dot_config/lvim/lua/user/autocommands.lua
@@ -0,0 +1,34 @@
+local M = {}
+
+M.chezmoi = function()
+-- Configure vim to run chezmoi apply when a dotfile is saved
+-- source: https://www.chezmoi.io/user-guide/tools/editor/#configure-vim-to-run-chezmoi-apply-whenever-you-save-a-dotfile
+  if vim.fn.executable('chezmoi') == 1 then
+    vim.api.nvim_create_augroup('Chezmoi', {
+      event = 'BufWritePost',
+      pattern = '~/.local/share/chezmoi/*',
+      command = [[silent! chezmoi apply --source-path "%"]],
+      desc = 'apply dotfiles modifications with chezmoi if executable',
+    })
+  end
+end
+
+M.hocon = function()
+  local hocon_group = vim.api.nvim_create_augroup("hocon", { clear = true })
+  vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
+    group = hocon_group,
+    pattern = '*/resources/*.conf',
+    command = 'set ft=hocon'
+  })
+end
+
+M.drools = function()
+  local drools_group = vim.api.nvim_create_augroup("drools", {clear = true})
+  vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
+    group = drools_group,
+    pattern = { '*.drl', '*.rdrl' },
+    command = 'set ft=drools'
+  })
+end
+
+return M
