615829a49bf64d65357dea7c1a14738d60a917c4
TJ Kolleh <tinatuh@gmail.com>
2024-06-21 14:50:33 -0400

    Better line wrap

--- private_dot_config/lvim/lua/user/autocommands.lua
+++ private_dot_config/lvim/lua/user/autocommands.lua
@@ -17,7 +17,7 @@ M.hocon = function()
   local hocon_group = vim.api.nvim_create_augroup("hocon", { clear = true })
   vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
     group = hocon_group,
-    pattern = '*/resources/*.conf',
+    pattern = '*.conf',
     command = 'set ft=hocon'
   })
 end
