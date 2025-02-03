490faf1c525d4f2344d38d420358e1ce03ad00c1
TJ Kolleh <tj@kolleh.com>
2025-02-02 21:37:48 -0500

    Update readme

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -3,6 +3,7 @@
 -- Add any additional keymaps here
 
 local utils = require("utils")
+local unimpaired = require("utils.unimpaired")
 
 local function _map(mode, lhs, rhs, opts)
   local keys = require("lazy.core.handler").handlers.keys
@@ -15,6 +16,7 @@ local function _map(mode, lhs, rhs, opts)
   end
 end
 
+-- Escape to normal with 'jk'
 _map({ "i", "v" }, "jk", "<ESC>")
 
 --
@@ -24,6 +26,59 @@ _map({ "i", "v" }, "jk", "<ESC>")
 _map({ "n", "v" }, "gs", "^") -- Go to first non-whitespace character of the line
 _map({ "n", "v" }, "gh", "0") -- Goto start of line
 _map({ "n", "v" }, "gl", "$") -- Goto end of line
+
+--
+-- Keymaps similar to vim unimpaired
+-- See: https://github.com/tpope/vim-unimpaired/blob/master/doc/unimpaired.txt
+--
+-- Toggles only
+Snacks.toggle({
+  name = "Toggle Background",
+  get = utils.is_background_dark,
+  set = function(state)
+    if state then
+      utils.setDark()
+    else
+      utils.setLight()
+    end
+  end,
+}):map("yob")
+
+Snacks.toggle({
+  name = "Toggle line wrap",
+  get = unimpaired.is_wrapped,
+  set = function(state)
+    if state then
+      unimpaired.enable_wrap()
+    else
+      unimpaired.disable_wrap()
+    end
+  end,
+}):map("yow")
+
+Snacks.toggle({
+  name = "Toggle spell check",
+  get = unimpaired.is_spellchecked,
+  set = function(state)
+    if state then
+      unimpaired.enable_spell()
+    else
+      unimpaired.disable_spell()
+    end
+  end,
+}):map("yos")
+
+Snacks.toggle({
+  name = "Toggle relative line number",
+  get = unimpaired.is_relativenumber,
+  set = function(state)
+    if state then
+      unimpaired.enable_relativenumber()
+    else
+      unimpaired.disable_relativenumber()
+    end
+  end,
+}):map("yor")
 
 -- Disable default keymaps
 -- local del = vim.keymap.del
