acefb478dd709feaee4d96a9d332b0417beb7821
TJ Kolleh <tinatuh@gmail.com>
2025-03-04 22:04:43 -0500

    Filename fisrt in ivy

--- private_dot_config/nvim/lua/plugins/fzf-lua.lua
+++ private_dot_config/nvim/lua/plugins/fzf-lua.lua
@@ -2,12 +2,11 @@ return {
   {
     "ibhagwan/fzf-lua",
     config = function()
-      require("fzf-lua").setup({ { "ivy", "hide" } })
-    end,
-    opts = function(_, opts)
-      opts.defaults = vim.tbl_deep_extend("force", opts.defaults, {
-        formatter = "path.filename_first",
-        -- formatter = "path.dirname_first",
+      require("fzf-lua").setup({
+        { "ivy", "default" },
+        files = { formatter = "path.filename_first" },
+        grep = { formatter = "path.filename_first" },
+        oldfiles = { formatter = "path.filename_first" },
       })
     end,
   },
