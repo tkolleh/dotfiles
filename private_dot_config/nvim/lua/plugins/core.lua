3b8757b005caa364c72e1ca24dfa1fd2c63c4c4a
TJ Kolleh <tj@kolleh.com>
2025-03-04 22:04:43 -0500

    Filename fisrt in ivy

--- private_dot_config/nvim/lua/plugins/core.lua
+++ private_dot_config/nvim/lua/plugins/core.lua
@@ -13,6 +13,15 @@
 local utils = require("utils")
 
 return {
+  {
+    "s1n7ax/nvim-window-picker",
+    name = "window-picker",
+    event = "VeryLazy",
+    version = "2.*",
+    config = function()
+      require("window-picker").setup()
+    end,
+  },
   -- add gruvbox theme
   { "ellisonleao/gruvbox.nvim" },
 
