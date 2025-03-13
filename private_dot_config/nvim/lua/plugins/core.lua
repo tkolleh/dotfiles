5d775d579300755ddd27ed57b024d412be650f31
TJ Kolleh <tinatuh@gmail.com>
2025-03-13 01:59:13 -0400

    ideavimrc changes

--- private_dot_config/nvim/lua/plugins/core.lua
+++ private_dot_config/nvim/lua/plugins/core.lua
@@ -14,6 +14,34 @@ local utils = require("utils")
 
 return {
   {
+    "linrongbin16/gitlinker.nvim",
+    cmd = "GitLink",
+    opts = function(_, opts)
+      local routers = require("gitlinker.routers")
+      return vim.tbl_deep_extend("force", opts or {}, {
+        -- example: https://code.corp.creditkarma.com/ck-private/con_idl/blob/master/acquisition-context/shared/v1/acquisitionServiceBusV1.thrift#L66
+        router = {
+          browse = {
+            ["^code%.corp%.creditkarma%.com"] = routers.github_browse,
+          },
+          blame = {
+            ["^code%.corp%.creditkarma%.com"] = routers.github_blame,
+          },
+          default_branch = {
+            ["^code%.corp%.creditkarma%.com"] = routers.github_default_branch,
+          },
+          current_branch = {
+            ["^code%.corp%.creditkarma%.com"] = routers.github_current_branch,
+          },
+        },
+      })
+    end,
+    keys = {
+      { "<leader>gy", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Yank git link" },
+      { "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
+    },
+  },
+  {
     "s1n7ax/nvim-window-picker",
     name = "window-picker",
     event = "VeryLazy",
