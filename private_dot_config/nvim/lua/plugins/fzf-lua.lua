29e648c0bbeaf4c54e39359265239de335d7691d
tkolleh <tinatuh@gmail.com>
2025-03-27 02:10:44 -0400

    all kinds of stuff for fre

--- private_dot_config/nvim/lua/plugins/fzf-lua.lua
+++ private_dot_config/nvim/lua/plugins/fzf-lua.lua
@@ -3,7 +3,7 @@ return {
     "ibhagwan/fzf-lua",
     config = function()
       require("fzf-lua").setup({
-        -- { "ivy", "default" },
+        { "ivy", "fzf-native" },
         files = { formatter = "path.filename_first" },
         grep = { formatter = "path.filename_first" },
         oldfiles = { formatter = "path.filename_first" },
