091b4990e54c719a10035e1f7344ea78dc673533
TJ Kolleh <tinatuh@gmail.com>
2025-03-26 22:11:18 -0400

    sync it

--- private_dot_config/nvim/lua/plugins/fzf-lua.lua
+++ private_dot_config/nvim/lua/plugins/fzf-lua.lua
@@ -3,7 +3,7 @@ return {
     "ibhagwan/fzf-lua",
     config = function()
       require("fzf-lua").setup({
-        { "ivy", "default" },
+        -- { "ivy", "default" },
         files = { formatter = "path.filename_first" },
         grep = { formatter = "path.filename_first" },
         oldfiles = { formatter = "path.filename_first" },
