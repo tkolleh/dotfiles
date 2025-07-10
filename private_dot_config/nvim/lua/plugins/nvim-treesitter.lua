0f77117c523441a794f7d7bf084153a59709b171
TJ Kolleh <tinatuh@gmail.com>
2025-07-10 01:43:50 -0400

    simplify treesitter config

--- private_dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ private_dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -5,14 +5,37 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      -- add tsx and treesitter
-      vim.list_extend(opts.ensure_installed, {
+    opts = {
+      ensure_installed = {
+        "bash",
+        "c",
+        "diff",
+        "html",
+        "javascript",
+        "jsdoc",
+        "json",
+        "jsonc",
+        "lua",
+        "luadoc",
+        "luap",
+        "markdown",
+        "markdown_inline",
+        "printf",
+        "python",
+        "query",
+        "regex",
+        "toml",
         "tsx",
         "typescript",
+        "vim",
+        "vimdoc",
+        "xml",
+        "yaml",
         "editorconfig",
         "hocon",
-      })
-    end,
+        "scala",
+        "roc",
+      },
+    },
   },
 }
