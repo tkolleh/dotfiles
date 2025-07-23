2261ec00ef2074637ef7133d9646fe018ec53875
TJ Kolleh <tinatuh@gmail.com>
2025-07-22 22:58:16 -0400

    Autodark mode via terminal hook

--- private_dot_config/nvim/lua/config/autocmds.lua
+++ private_dot_config/nvim/lua/config/autocmds.lua
@@ -7,8 +7,8 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 --
-
 local api = vim.api
+local utils = require("utils")
 
 -- Hocon_group
 -- See: https://github.com/antosha417/tree-sitter-hocon
@@ -18,29 +18,17 @@ api.nvim_create_autocmd(
   { group = hocon_group, pattern = "*.conf", command = "set ft=hocon" }
 )
 
--- Function to show the column if a line exceeds the specified length
-local function show_column_if_line_too_long(length_limit)
-  local max_line_length = 0
-  for _, line in ipairs(api.nvim_buf_get_lines(0, 0, -1, false)) do
-    max_line_length = math.max(max_line_length, #line)
-  end
-
-  if max_line_length > length_limit then
-    vim.cmd("highlight ColorColumn ctermbg=red guibg=red")
-    vim.wo.colorcolumn = tostring(length_limit + 1)
-  else
-    vim.wo.colorcolumn = ""
-  end
-end
-
--- Create an augroup for line wraps
--- local line_wrap_guide = api.nvim_create_augroup("line_wrap_group", { clear = true })
--- -- Add autocommands to the group for drawing a column marking the maximum line length
--- api.nvim_create_autocmd(
---   { "BufRead", "TextChanged", "TextChangedI" },
---   {
---     group = line_wrap_guide,
---     pattern = { "*.js", "*.ts", "*.hs", "*.scala", "*.py", "*.java" },
---     callback = function() show_column_if_line_too_long(120) end,
---   }
--- )
+-- Auto light / dark theme
+-- The decision will be made based on system preferences using DEC mode 2031 if supported by the terminal.
+-- See: https://github.com/neovim/neovim/pull/31350
+api.nvim_create_autocmd('OptionSet', {
+  group = api.nvim_create_augroup('dec_auto_background', { clear = true }),
+  pattern = 'background',
+  callback = function()
+    if utils.is_background_dark() then
+      utils.setDark()
+    else
+      utils.setLight()
+    end
+  end,
+})
