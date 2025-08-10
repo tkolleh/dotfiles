eb7d2e0eb56d133e684590b08f350204f9c23518
TJ Kolleh <tinatuh@gmail.com>
2025-08-10 12:10:03 -0400

    style: Monrovia theme

--- private_dot_config/nvim/lua/plugins/nvim-metals.lua
+++ private_dot_config/nvim/lua/plugins/nvim-metals.lua
@@ -76,6 +76,13 @@ return {
     return metals_config
   end,
   config = function(self, metals_config)
-    require("metals").initialize_or_attach(metals_config)
+    local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = self.ft,
+      callback = function()
+        require("metals").initialize_or_attach(metals_config)
+      end,
+      group = nvim_metals_group,
+    })
   end,
 }
