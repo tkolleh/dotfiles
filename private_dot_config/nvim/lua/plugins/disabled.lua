ba20420a5c8943f0063f652a3494868fb3e599d9
TJ Kolleh <tinatuh@gmail.com>
2025-08-10 20:03:47 -0400

    Disable markdown plugin

--- private_dot_config/nvim/lua/plugins/disabled.lua
+++ private_dot_config/nvim/lua/plugins/disabled.lua
@@ -2,5 +2,7 @@ return {
   -- disable flash
   -- prefer vim motions and text objects to get around
   { "folke/flash.nvim", enabled = false },
+  { "iamcco/markdown-preview.nvim", enabled = false },
+  { "markdown-preview.nvim", enabled = false },
   { 'MeanderingProgrammer/render-markdown.nvim', enabled = false},
 }
