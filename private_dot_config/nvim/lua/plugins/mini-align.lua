16a50ecf5d6a7735ab86f5728b463460ef405710
TJ Kolleh <tinatuh@gmail.com>
2025-09-04 21:42:30 -0400

    include asciidoc tooling

---
+++ private_dot_config/nvim/lua/plugins/mini-align.lua
@@ -0,0 +1,9 @@
+return {
+  'echasnovski/mini.align',
+  version = '*',
+  keys = {
+      { 'ga', mode = { 'n', 'x' } },
+      { 'gA', mode = { 'n', 'x' } },
+  },
+  opts = {},
+}
---
+++ private_dot_config/nvim/lua/plugins/tree-sitter-d2.lua
@@ -0,0 +1,5 @@
+return {
+  "ravsii/tree-sitter-d2",
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  build = "make nvim-install",
+}
