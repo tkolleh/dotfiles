1eb025777840ca4d7609981ca11a370a032e097c
TJ Kolleh <tj@kolleh.com>
2025-09-12 09:21:43 -0400

    d2 is cool

--- private_dot_config/nvim/lua/plugins/d2-vim.lua
+++ private_dot_config/nvim/lua/plugins/d2-vim.lua
@@ -1,4 +1,11 @@
 return {
   "terrastruct/d2-vim",
   ft = { "d2" },
+  keys = {
+    -- disable default keymaps and prefer the commands
+    {"<Leader>d2", false}, -- Render selected text as ASCII (visual mode, any file)
+    {"<Leader>d2", false}, -- Render entire buffer as ASCII (normal mode, D2 files only)
+    {"<Leader>rd", false}, -- Replace selected D2 code with ASCII render (visual mode, any file)
+    {"<Leader>yd", false}, -- Copy ASCII preview content to clipboard and yank register (normal mode, any file)
+  }
 }
