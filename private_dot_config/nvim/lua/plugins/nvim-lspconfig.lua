4278c0f08fbde610dece5688071b2cb4966e2dec
tkolleh <tinatuh@gmail.com>
2025-09-20 23:50:06 -0400

    style: disable lsp config text lines

--- private_dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ private_dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,6 +1,8 @@
 return {
   "neovim/nvim-lspconfig",
   opts = function(_, opts)
+    opts.diagnostics.virtual_text = false
+    opts.diagnostics.virtual_lines = false
     opts.servers.metals = nil
     opts.setup.metals = nil
     return opts
