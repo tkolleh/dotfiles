e3fef5b275cb591da0abab72c02d26e6383668b4
tkolleh <tinatuh@gmail.com>
2026-02-17 01:13:36 -0500

    fix: nvim providers

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -36,6 +36,17 @@ end
 -- 4. Set Host Prog (Essential for Neovim python provider)
 vim.g.python3_host_prog = python_executable
 
+-- Node Configuration
+-- Explicitly set node host prog to avoid checkhealth warnings with volta/nvm
+local node_host = vim.fn.exepath("neovim-node-host")
+if node_host ~= "" then
+  vim.g.node_host_prog = node_host
+end
+
+-- Disable unused providers
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+
 -- Raise the bar a bit
 vim.opt.cmdheight = 1
 
