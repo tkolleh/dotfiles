9082d91660e3adc0da04db6c8333612a627b2fd5
TJ Kolleh <tinatuh@gmail.com>
2026-02-17 02:01:14 -0500

    fix: Remove unused plugin

--- private_dot_config/nvim/lua/plugins/telescope-nvim.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "nvim-telescope/telescope.nvim",
-  opts = {
-    defaults = {
-      layout_strategy = "vertical",
-      layout_config = { prompt_position = "top", width = 0.9, height = 0.9 },
-    },
-  },
-}
