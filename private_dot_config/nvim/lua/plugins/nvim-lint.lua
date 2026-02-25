76d1edf284c46affeca65f4f510788d5ccf7e0a2
TJ Kolleh <tinatuh@gmail.com>
2026-02-25 08:40:38 -0500

    fix: Ignore jsonc

--- private_dot_config/nvim/lua/plugins/nvim-lint.lua
+++ private_dot_config/nvim/lua/plugins/nvim-lint.lua
@@ -2,11 +2,31 @@
 -- Run installed linters
 -- Customize default linter
 return {
-  {
-    "mfussenegger/nvim-lint",
-    opts = function(_, opts)
-      -- Removed invalid markdownlint config
-      return opts
-    end,
-  },
+  "mfussenegger/nvim-lint",
+  opts = {
+    linters_by_ft = {
+      fish = { "fish" },
+      -- Use the "*" filetype to run linters on all filetypes.
+      -- ['*'] = { 'global linter' },
+      -- Use the "_" filetype to run linters on filetypes that don't have other linters configured.
+      -- ['_'] = { 'fallback linter' },
+      -- ["*"] = { "typos" },
+    },
+    -- LazyVim extension to easily override linter options
+    -- or add custom linters.
+    ---@type table<string,table>
+    linters = {
+      -- -- Example of using selene only when a selene.toml file is present
+      -- selene = {
+      --   -- `condition` is another LazyVim extension that allows you to
+      --   -- dynamically enable/disable linters based on the context.
+      --   condition = function(ctx)
+      --     return vim.fs.find({ "selene.toml" }, { path = ctx.filename, upward = true })[1]
+      --   end,
+      -- },
+      markdownlint = {
+        args = { "--disable", "MD012", "MD013", "MD033", "MD026", "--" },
+      }
+    },
+  }
 }
