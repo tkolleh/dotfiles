bf5362738152e0bc559f169d92f47e9b679e4a0f
TJ Kolleh <tinatuh@gmail.com>
2026-04-10 17:22:27 -0400

    chore: Remove unused

--- private_dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ private_dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,8 +1,13 @@
 return {
   "neovim/nvim-lspconfig",
   opts = function(_, opts)
-    opts.diagnostics.virtual_text = false
-    opts.diagnostics.virtual_lines = false
+    -- Single source of truth for diagnostic display.
+    -- Uses Neovim 0.12 current_line to show full detail on cursor line only.
+    opts.diagnostics = vim.tbl_deep_extend("force", opts.diagnostics or {}, {
+      underline = false,
+      virtual_text = { current_line = false },
+      virtual_lines = { current_line = true },
+    })
     opts.servers.metals = nil
     opts.setup.metals = nil
     return opts
